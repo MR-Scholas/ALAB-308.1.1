d051e425d4265cb05c443a37bfc38c001a4bab72
MR-Scholas <160638944+MR-Scholas@users.noreply.github.com>
2024-03-13 13:23:49 -0400

    divisible by 5 check

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Data Manipulation with JavaScript
 
 // The initial numbers that must be verified.
-const n1 = 10;
+const n1 = 11;
 const n2 = 15;
 const n3 = 20;
 const n4 = 5;
@@ -45,3 +45,21 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
   ((n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2) && 
   !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
   (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
+
+//Check if all numbers are divisible by 5. Cache the result in a variable.
+    const isDivisibleBy5 = ( n1 % 5 == 0 && n2 % 5 == 0 && n3 % 5 == 0 && n4 % 5 == 0 )
+    console.log("Divisible by 5 = "+isDivisibleBy5)
+
+//Check if the first number is larger than the last. Cache the result in a variable.
+
+
+//Accomplish the following arithmetic chain:
+//Subtract the first number from the second number.
+//Multiply the result by the third number.
+//Find the remainder of dividing the result by the fourth number.
+
+
+//Change the way that isOver25 calculates so that we do not need to use the NOT operator (!) in other logic comparisons. Rename the variable as appropriate.
+
+
+  
